78d0ef9b45a1dab781a0215fc3eb895dc8b54b40
ShadøwXPA <pedro.aves@hotmail.com>
2021-06-13 21:01:35 +0100

    stuff

--- Neural Network/GetImages.m
+++ Neural Network/GetImages.m
@@ -1,11 +1,36 @@
-function [images] = GetImages(dirName)
-    files = dir(dirName);
-    numFiles = length(files);
-    resolution = 21;
-    images = zeros(resolution, resolution, numFiles, 'uint8');
-    for i = 1 : numFiles
-        image = imread(strcat(files(i).folder, '\', files(i).name));
-        image = imresize(image, [resolution resolution]);
-        images(:,:,i) = image;
-    end
+function [imageInput, imageTarget] = GetImages(dirName, filter, reverse)
+if (~exist('reverse', 'var'))
+    reverse = 0;
+end
+
+numImageTypes = 10;
+
+files = dir(dirName);
+numFiles = length(files);
+
+numFilesPerType = numFiles / numImageTypes;
+
+fileNames = strings(1, numFiles);
+for i = 1 : numFiles
+    fileNames(i) = files(i).name;
+end
+
+% SORT FILES
+num = cellfun(@(x) sscanf(x, filter), fileNames);
+[~, sortindex] = sort(num);
+fileNames2 = fileNames(sortindex);
+
+if (reverse == 1)
+    fileNames2 = fliplr(fileNames2);
+end
+resolution = 21;
+imageInput = zeros(resolution * resolution, numFiles);
+imageTarget = zeros(numImageTypes, numFiles);
+for i = 1 : numFiles
+    image = imread(strcat(files(i).folder, '\', fileNames2(i)));
+    image = imresize(image, [resolution resolution]);
+    imageInput(:,i) = image(:);
+    j = floor((i - 1) / numFilesPerType) + 1;
+    imageTarget(j, i) = 1;
+end
 end
--- Neural Network/Main.m
+++ Neural Network/Main.m
@@ -1,10 +1,119 @@
 function Main()
-    clear;
-    close all;
+clear;
+close all;
 
-    a();
+a();
+% b("savedNetwork.mat");
 end
 
 function a()
-    images = GetImages('Trabalho_Pratico_CR_2020_21/Pasta1/*.jpg');
+% Carregar imagens
+%     [imageInput, imageTarget] = GetImages('Pasta2/*.jpg', 'letter_bnw_%d', 1);
+[imageInput, imageTarget] = GetImages('Pasta1/*.jpg', '%d');
+
+% Criar rede neuronal
+net = feedforwardnet([10]);
+
+% Função de ativação
+net.layers{2}.transferFcn = 'tansig';
+
+% Função de treino
+net.trainFcn = 'traingdx';
+
+% Número de épocas
+net.trainParam.epochs = 1000;
+
+% 
+net.divideFcn = '';
+
+% Treino
+[net, tr] = train(net, imageInput, imageTarget);
+
+% Simular
+%     [imageInput, imageTarget] = GetImages('Pasta1/*.jpg', '%d');
+y = sim(net, imageInput);
+
+% accuracy = CalculateAccuracy(y, imageTarget);
+% fprintf('Precisão total: %f\n', accuracy);
+%
+% accuracyTest = CalculateAccuracy(y(:, tr.testInd), imageTarget(:, tr.testInd));
+% fprintf('Precisão teste: %f\n', accuracyTest);
+
+plotconfusion(imageTarget, y);
+end
+
+function b(filename)
+transferFcns = ['compet',...
+    'hardlim',...
+    'hardlims',...
+    'logsig',...
+    'netinv',...
+    'poslin',...
+    'purelin',...
+    'radbas',...
+    'radbasn',...
+    'satlin',...
+    'satlins',...
+    'softmax',...
+    'tansig',...
+    'tribas'];
+
+trainFcns = ['trainbfg',...
+    'trainbfgc',...
+    'trainbr',...
+    'trainbu',...
+    'trainc',...
+    'traincgb',...
+    'traincgf',...
+    'traincgp',...
+    'traingd',...
+    'traingda',...
+    'traingdm',...
+    'traingdx',...
+    'trainlm',...
+    'trainoss',...
+    'trainr',...
+    'trainrp',...
+    'trainru',...
+    'trains'];
+
+if (~exist(filename, 'file'))
+    % Carregar imagens
+    [imageInput, imageTarget] = GetImages('Pasta2/*.jpg', 'letter_bnw_%d', 1);
+    %     [imageInput, imageTarget] = GetImages('Pasta1/*.jpg', '%d');
+    
+    % Criar rede neuronal
+    net = feedforwardnet([10]);
+    
+    % Função de ativação
+    net.layers{2}.transferFcn = 'tansig';
+    
+    % Função de treino
+    net.trainFcn = 'traingdx';
+    
+    % Número de épocas
+    net.trainParam.epochs = 1000;
+    
+    %
+    net.divideFcn = '';
+    
+    % Treino
+    [net, tr] = train(net, imageInput, imageTarget);
+    
+    % Simular
+    %     [imageInput, imageTarget] = GetImages('Pasta1/*.jpg', '%d');
+    y = sim(net, imageInput);
+    
+    accuracy = CalculateAccuracy(y, imageTarget);
+    fprintf('Precisão total: %f\n', accuracy);
+    
+    accuracyTest = CalculateAccuracyTest(y, imageTarget, tr);
+    fprintf('Precisão teste: %f\n', accuracyTest);
+else
+    load(filename);
+end
+
+save(filename);
+
+plotconfusion(imageTarget, y);
 end
