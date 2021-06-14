f055cfcd72f09042879e214791be95143fb3c03a
ShadøwXPA <pedro.aves@hotmail.com>
2021-06-14 18:33:09 +0100

    redes guardadas

--- Neural Network/Main.m
+++ Neural Network/Main.m
@@ -2,8 +2,8 @@ function Main()
 clear;
 close all;
 
-a();
-% b("savedNetwork.mat");
+% a();
+b();
 end
 
 function a()
@@ -23,7 +23,7 @@ net.trainFcn = 'traingdx';
 % Número de épocas
 net.trainParam.epochs = 1000;
 
-% 
+%
 net.divideFcn = '';
 
 % Treino
@@ -42,78 +42,83 @@ y = sim(net, imageInput);
 plotconfusion(imageTarget, y);
 end
 
-function b(filename)
-transferFcns = ['compet',...
-    'hardlim',...
-    'hardlims',...
-    'logsig',...
-    'netinv',...
-    'poslin',...
-    'purelin',...
-    'radbas',...
-    'radbasn',...
-    'satlin',...
-    'satlins',...
-    'softmax',...
-    'tansig',...
-    'tribas'];
+function b()
+% Carregar imagens
+[imageInput, imageTarget] = GetImages('Pasta2/*.jpg', 'letter_bnw_%d', 1);
 
-trainFcns = ['trainbfg',...
-    'trainbfgc',...
-    'trainbr',...
-    'trainbu',...
-    'trainc',...
-    'traincgb',...
-    'traincgf',...
-    'traincgp',...
-    'traingd',...
-    'traingda',...
-    'traingdm',...
-    'traingdx',...
-    'trainlm',...
-    'trainoss',...
-    'trainr',...
-    'trainrp',...
-    'trainru',...
-    'trains'];
+layers = [{[10]},...
+    {[20]},...
+    {[5 5]},...
+    {[10 10]}];
 
-if (~exist(filename, 'file'))
-    % Carregar imagens
-    [imageInput, imageTarget] = GetImages('Pasta2/*.jpg', 'letter_bnw_%d', 1);
-    %     [imageInput, imageTarget] = GetImages('Pasta1/*.jpg', '%d');
-    
-    % Criar rede neuronal
-    net = feedforwardnet([10]);
-    
-    % Função de ativação
-    net.layers{2}.transferFcn = 'tansig';
-    
-    % Função de treino
-    net.trainFcn = 'traingdx';
-    
-    % Número de épocas
-    net.trainParam.epochs = 1000;
-    
-    %
-    net.divideFcn = '';
-    
-    % Treino
-    [net, tr] = train(net, imageInput, imageTarget);
-    
-    % Simular
-    %     [imageInput, imageTarget] = GetImages('Pasta1/*.jpg', '%d');
-    y = sim(net, imageInput);
-    
-    accuracy = CalculateAccuracy(y, imageTarget);
-    fprintf('Precisão total: %f\n', accuracy);
-    
-    accuracyTest = CalculateAccuracyTest(y, imageTarget, tr);
-    fprintf('Precisão teste: %f\n', accuracyTest);
-else
-    load(filename);
+transferFcns = [{'logsig'},...
+    {'purelin'},...
+    {'tansig'}];
+
+%'trainbfg',...
+trainFcns = [{'traingd'},...
+    {'traingdx'},...
+    {'trainlm'}];
+
+divideFcns = [{'dividerand'},...
+    {'divideblock'},...
+    {'divideint'}];
+
+divideParams = [{[0.7 0.15 0.15]},...
+    {[0.8 0.1 0.1]},...
+    {[0.6 0.2 0.2]}];
+
+sizeOfLayers = size(layers, 2);
+sizeOfTransfer = size(transferFcns, 2);
+sizeOfTrain = size(trainFcns, 2);
+sizeOfDivide = size(divideFcns, 2);
+sizeOfParams = size(divideParams, 2);
+
+for lay = 1 : sizeOfLayers
+    for trans = 1 : sizeOfTransfer
+        for trai = 1 : sizeOfTrain
+            for divi = 1 : sizeOfDivide
+                for param = 1 : sizeOfParams
+                    fprintf(string(strjoin(["bestNetwork" layers{lay} transferFcns{trans} trainFcns{trai} divideFcns{divi} divideParams{param} ".mat"])));
+                    
+                    % Criar rede neuronal
+                    net = feedforwardnet(layers{lay});
+                    
+                    % Função de ativação
+                    act = size(layers{lay}, 2) + 1;
+                    net.layers{act}.transferFcn = transferFcns{trans};
+                    
+                    % Função de treino
+                    net.trainFcn = trainFcns{trai};
+                    
+                    % Número de épocas
+                    net.trainParam.epochs = 500;
+                    
+                    %
+                    net.divideFcn = divideFcns{divi};
+                    net.divideParam.trainRatio = divideParams{param}(1);
+                    net.divideParam.valRatio = divideParams{param}(2);
+                    net.divideParam.testRatio = divideParams{param}(3);
+                    
+                    % Treino
+                    [net, tr] = train(net, imageInput, imageTarget);
+                    
+                    % Simular
+                    %     [imageInput, imageTarget] = GetImages('Pasta1/*.jpg', '%d');
+                    y = sim(net, imageInput);
+                    
+                    accuracy = CalculateAccuracy(y, imageTarget);
+                    fprintf('\nPrecisão total: %f\n', accuracy);
+                    
+                    accuracyTest = CalculateAccuracyTest(y, imageTarget, tr);
+                    fprintf('Precisão teste: %f\n', accuracyTest);
+                    
+                    fileName = string(strjoin(["RedesGravadas/bestNetworkB" layers{lay} transferFcns{trans} trainFcns{trai} divideFcns{divi} divideParams{param} ".mat"]));
+                    SaveBestNetwork(fileName, net, accuracy, accuracyTest);
+                    SaveBestNetwork("RedesGravadas/bestNetworkB.mat", net, accuracy, accuracyTest);
+                end
+            end
+        end
+    end
 end
-
-save(filename);
-
-plotconfusion(imageTarget, y);
 end
